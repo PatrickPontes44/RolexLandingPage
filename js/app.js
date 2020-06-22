90f41b702097c6ac92bbd3ef7bbf40609d2eff02
Patrick Pontes <patrickhisoka@gmail.com>
2020-06-21 21:47:14 -0300

    adicionada nova seção

--- js/app.js
+++ js/app.js
@@ -1,7 +1,6 @@
 //Pegar o tamanho da tela.
 const TELA_HTML = document.documentElement.clientWidth;
 
-/* When the user scrolls down, hide the navbar. When the user scrolls up, show the navbar */
 var prevScrollpos = window.pageYOffset;
 window.onscroll = function() {
   var currentScrollPos = window.pageYOffset;
@@ -18,7 +17,6 @@ window.onscroll = function() {
 }
 
 
-/* Set the width of the side navigation to 250px and the left margin of the page content to 250px and add a black background color to body */
 function openNav() {
   if(TELA_HTML <= 650){
     document.getElementById("side-menu").style.width = "100vw";
@@ -31,7 +29,6 @@ function openNav() {
   document.getElementById("menu-options").style.visibility = "visible";
 }
 
-/* Set the width of the side navigation to 0 and the left margin of the page content to 0, and the background color of body to white */
 function closeNav() {
   document.getElementById("menu-header").style.visibility = "hidden";
   document.getElementById("menu-options").style.visibility = "hidden";
